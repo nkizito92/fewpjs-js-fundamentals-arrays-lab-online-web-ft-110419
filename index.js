1dbcca978add545a130a61bd247dde3af16c5480
Kizito Njoku <nkizito57@gmail.com>
2020-03-02 18:58:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"];
+
+append.push('Odie');
+
+const prepend = [...append.slice(0, 4)];
+prepend.pop('Odie');
+
+prepend.unshift('Odie');
+
+const removeLast = [...prepend.splice(1, 2, 'Milo', 'Otis')];
+
+
+const removeFirst = [...removeLast, 'Garfield']
+
+removeFirst.shift(0)
